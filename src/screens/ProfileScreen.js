1720e8b25c4b7dcf6f2e574c9e8c5ac6128ecff0
Mahendrashahh <shahmahendra380@gmail.com>
2026-02-18 22:27:56 +0530

    My feature code

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,93 @@
+
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 
 export default function ProfileScreen() {
   return (
-    <SafeAreaView style={{ flex: 1, padding: 12 }}>
-      <Text>Profile</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      
+      {/* Avatar */}
+      <Image
+        source={{ uri: "https://i.pravatar.cc/300" }}
+        style={styles.avatar}
+      />
+
+      {/* Welcome Text */}
+      <Text style={styles.title}>Welcome 👋</Text>
+      <Text style={styles.subtitle}>We are happy to see you here</Text>
+
+      {/* Buttons */}
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Settings</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Login</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn}>
+        <Text style={styles.btnText}>Terms & Policy</Text>
+      </TouchableOpacity>
+
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+     backgroundColor: "#d2d8e7",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 20,
+  },
+
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 100,
+    marginBottom: 20,
+    borderWidth: 3,
+    borderColor: "#57e48b",
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    color: "#141111",
+  },
+
+  subtitle: {
+    fontSize: 14,
+    color: "#0b1929",
+    marginBottom: 30,
+  },
+
+  btn: {
+    width: "80%",
+     backgroundColor: "#6366f1",
+    padding: 14,
+    borderRadius: 12,
+    marginVertical: 8,
+    alignItems: "center",
+  },
+
+  btnText: {
+    color: "#ffffff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/screens/userdashborad.js
@@ -0,0 +1,149 @@
+
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+export default function ProfileDetails({ navigation }) {
+
+  const [name, setName] = useState("Mahendra");
+  const [email, setEmail] = useState("mahendra@gmail.com");
+  const [branch, setBranch] = useState("IT");
+  const [bio, setBio] = useState("Dream big 🚀");
+
+  const [edit, setEdit] = useState(false);
+
+  const [image, setImage] = useState("https://i.pravatar.cc/300");
+
+  // pick profile image
+  const pickImage = async () => {
+    if (!edit) return;
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>My Profile</Text>
+
+      {/* PROFILE IMAGE */}
+      <TouchableOpacity onPress={pickImage}>
+        <Image source={{ uri: image }} style={styles.avatar} />
+        {edit && <Text style={styles.changeText}>Tap to change</Text>}
+      </TouchableOpacity>
+
+      {/* NAME */}
+      <TextInput
+        style={styles.input}
+        value={name}
+        editable={edit}
+        onChangeText={setName}
+      />
+
+      {/* EMAIL */}
+      <TextInput
+        style={styles.input}
+        value={email}
+        editable={edit}
+        onChangeText={setEmail}
+      />
+
+      {/* BRANCH */}
+      <TextInput
+        style={styles.input}
+        value={branch}
+        editable={edit}
+        onChangeText={setBranch}
+      />
+
+      {/* BIO */}
+      <TextInput
+        style={[styles.input,{height:80}]}
+        value={bio}
+        editable={edit}
+        onChangeText={setBio}
+        multiline
+      />
+
+      {/* EDIT BUTTON */}
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => setEdit(!edit)}
+      >
+        <Text style={styles.btnText}>
+          {edit ? "Save Profile" : "Edit Profile"}
+        </Text>
+      </TouchableOpacity>
+
+      {/* LOGOUT */}
+      <TouchableOpacity
+        style={[styles.btn,{backgroundColor:"#4d5a9d"}]}
+        onPress={() => navigation.replace("Login")}
+      >
+        <Text style={styles.btnText}>Logout</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container:{ flex:1,  backgroundColor: "#a0b1dc", padding:20 },
+
+  title:{
+    color:"#181414",
+    fontSize:26,
+    fontWeight:"bold",
+    marginBottom:20,
+    textAlign:"center"
+  },
+
+  avatar:{
+    width:120,
+    height:120,
+    borderRadius:100,
+    alignSelf:"center",
+    marginBottom:10,
+    borderWidth:3,
+    borderColor:"#5d7a88"
+  },
+
+  changeText:{
+    color:"#181616",
+    textAlign:"center",
+    marginBottom:20
+  },
+
+  input:{
+    borderWidth:1,
+    borderColor:"#636c6e",
+    padding:12,
+    borderRadius:10,
+    marginBottom:15,
+    color:"#200e0e"
+  },
+
+  btn:{
+    backgroundColor:"#504bb8",
+    padding:15,
+    borderRadius:10,
+    marginTop:10,
+    alignItems:"center"
+  },
+
+  btnText:{ color:"#fff", fontWeight:"bold" }
+});
